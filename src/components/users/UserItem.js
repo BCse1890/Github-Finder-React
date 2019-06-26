840585c0c31aba0eb0f11f090386abd4dbfe98e4
BCse1890 <bob.currie@fsresearch.co.uk>
2019-06-26 20:41:29 +0100

    Convert Navbar and userItem to Stateless Functional Components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    //   pull the values from the user that passed as prop to UserItem
-    const { login, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, html_url } }) => {
+  //   pull the values from the user that passed as prop to UserItem
+  return (
+    <div className="card text-center">
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 export default UserItem;
