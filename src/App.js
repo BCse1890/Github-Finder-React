6d63029e57b53cf5309332421ec6c2b766cb0fc2
BCse1890 <bob.currie@fsresearch.co.uk>
2019-06-27 21:56:43 +0100

    add Repos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -58,6 +59,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users' repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -74,7 +87,7 @@ class App extends Component {
     // can destructure as this line to pull out users and loading
     // and we can remove this.state from code below - also doing same
     // with props if we wish to
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
     return (
       // add Router by embedding in BrowserRouter
       <BrowserRouter>
@@ -107,7 +120,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
