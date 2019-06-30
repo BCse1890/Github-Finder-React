728dcbf0caf9a6356f9955812275cff4ca70d4dd
BCse1890 <bob.currie@fsresearch.co.uk>
2019-06-30 17:34:52 +0100

    final version to deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/pages/Home";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import PageNotFound from "./components/pages/PageNotFound";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -36,6 +37,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={PageNotFound} />
               </Switch>
             </div>
           </div>
